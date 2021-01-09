28f1f67deff3ef309cf6759b7f3267da0dc8fb93
issa <ikbalissa89@gmail.com>
2021-01-09 16:51:00 +0000

    save and load from a file

--- stock.cpp
+++ stock.cpp
@@ -2,22 +2,23 @@
 #include <stdio.h>
 #include <process.h>
 #include <fstream>
-
 #include <stdlib.h>
+
+
 using namespace std;
 
 class item
 {
 	int ItmNumber;
-	char ItmName[100];
+	char ItmName[50];
 	float Price,quantity;
 
 public:
 	// a class function to get the data
 	void insertdata()
 	{
-		 system("clear");
-		cout << "\nEnter The Item ID or Name --> \t";
+		 // system("clear");
+		cout << "\nEnter The Item ID --> \t";
 		cin >> ItmNumber;
 		cout << "\nEnter The Name of The Item -->\t";
 		cin >> ItmName;
@@ -27,10 +28,10 @@ public:
 	// this class will show the data
 	void displaydata()
 	{
-		system("clear");
-		cout << "\n The Item Number : " << ItmNumber;
-		cout << "\n The Name of The Item : " << ItmName;
-		cout << "\n The Price of The Item : " << Price;
+		//system("clear");
+		cout<<"\n The Item ID : " <<ItmNumber;
+		cout<<"\n The Name of The Item : " <<ItmName;
+		cout<<"\n The Price of The Item : " <<Price;
 	}
 
 	// a return function to return the private-
@@ -46,10 +47,8 @@ public:
 		return Price;
 	}
 
-	char *retItmName()
-	{
-		return ItmName;
-	}
+	char* RetItmName()
+	{return ItmName;}
 
 	// the end of the class
 }; 
@@ -61,142 +60,152 @@ item i;
 
 void write_file()
 {
-	   f.open("stroe.dat",ios::out|ios::app);
+	
+	   f.open("store.txt",ios::app|ios::app);
 	   i.insertdata();
 	   f.write((char*)&i,sizeof(i));
 	   f.close();
 	   cout<<"\n\nThe Iten Has Benn Created";
 
 }
+
 // a function to read records from file
 void display_all()
-{
-	system("clear");
-	cout<<"\n\n\n\t\tDisplay All Records\n\n";
-	f.open("store.dat",ios::in);
-	while(f.read((char*)&i,sizeof(i)));
+{  
+  //  system("clear")
+	cout<<"\n\n\n\t\t All Records\n\n";
+	f.open("store.txt",ios::in);
+	while(f.read((char*)&i,sizeof(i)))
 	{
 		i.displaydata();
 		cout<<"\n\n============================\n";
+		
+	}
 	
+	f.close();
+	
+}
 
+void menu()
+{
+	//system("clear");
+	f.open("store.txt",ios::in);
+	if(!f)
+	{
+		cout<<"ERROR ! file couldnt be open\n\n\n you need to creat an item ";
+        cout<<"\n\n\n program is closing";
+		
+		exit(0);
+		
+	}
+          
+	cout<<"\n\n\n\t\t Item Menu\n\n";
+	cout<<"=========================================\n";
+	cout<<"Item ID         NAME            Price\n";
+	cout<<"=========================================\n";
+
+	while (f.read((char*)&i,sizeof(i)))
+	{
+		cout<<i.RetItmNumber()<<"\t\t"<<i.RetItmName()<<"\t\t"<<i.RetPrice()<<endl;
 	}
 	f.close();
 }
-// to read from a specific record from a file
-void display_specific(int n)
-{
- int flag=0;
- f.open("store.dat",ios::in);
- while(f.read((char*)&i,sizeof(i)));
- {
-	if(i.RetItmNumber()==n)
-	{
-		system("clear");
-		i.displaydata();
-		flag=1;
-	}
- }
- f.close();
- if(flag==0)
- cout<<"\n\nThere is no Record";
- 
-}
 
 
-// a function to place an order
+//  a function to place an order
+
 void place_order()
 {
-	 system("clear");
-
-	int sell[100], quantity[100],c=0;
-	float amt,damt,total=0;
+//	system("clear");
+	int sell[50], quantity[50],c=0;
+	float amt,total=0;
 	char ch='Y';
+	menu();
 	cout<<"\n-------------";
-	cout<<"\n  Sell Items ";
+	cout<<"\n Sell Items ";
 	cout<<"\n-------------";
 	do
 	{
-		cout<<"\n\n Enter Item ID : \n";
+		cout<<"\n\nEnter Item ID : \n";
 		cin>>sell[c];
-		cout<<"\n Enter Quantity : \n";
+		cout<<"\nEnter Quantity : \n";
 		cin>>quantity[c];
 		c++;
-		cout<<"\n PLace Another Item ?  (Y/N) \t";
+		cout<<"\nPLace Another Item ?  (Y/N) \t";
 		cin>>ch;
-	} while (ch=='Y'   ||ch=='N');
-	//  system("clear");
-	cout<<"\n\n *******RECIPET******\n";
-	cout<<"\nItem No.\tName\tQuantity \tPrice \tAmount\n";
+	} while (ch=='y'||ch=='Y');
+	  // system("clear");
+	cout<<"\n\n****************RECIPET***************\n";
+	cout<<"\nID    Name   Quantity   Price   Amount\n";
 	for(int x=0;x<=c;x++)
 	{
-		f.open("store.dat",ios::in);
+		f.open("store.txt",ios::in);
 		f.read((char*)&i,sizeof(i));
 		while(!f.eof())
 		{
 			if(i.RetItmNumber()==sell[x])
 			{
 				amt=i.RetPrice()*quantity[x];
-				cout<<"\n"<<sell[x]<<"\t"<<i.RetItmNumber()<<"\t"<<quantity[x]<<"\t\t"<<i.RetPrice()<<"\t"<<amt<<"\t\t"<<damt;
-                total+=damt;
+				
+				cout<<"\n"<<sell[x]<<"      "<<i.RetItmName()<<"\t"<<quantity[x]<<"\t"<<i.RetPrice()<<"\t"<<amt;
+                total+=amt;
 			}
 			f.read((char*)&i,sizeof(i));
 		}
 		f.close();
 
 	}
-	cout<<"\n\n\t\t\t\t\t\t Total = " <<total;
+	cout<<"\n\n\t\t\t\t Total = "<<total;
 }
+
 
 int main()
 {
+	char ch;
 	
-		 system("clear");
-		char ch;
-		cout << "\n\n\n\t Stock Mangement System";
-		cout << "\n\n\t 1. Sell items";
-		cout << "\n\n\t 2. Add new items ";
-		cout << "\n\n\t 5. Display items";
-		cout << "\n\n\t 3. Restock items";
-		cout << "\n\n\t 4. Modify items ";
-		cout << "\n\n\t 6. Delete items";
-		cout << "\n\n\t 7. Exit";
-		cout << "\n\n\t please select (1-7) \t";
+	do{
+		//system("clear");
+		cout << "\n\n\n======================";
+		cout << "\nStock Mangement System";
+		cout << "\n======================";
+		cout << "\n      Music Shop      ";
+		cout << "\n======================";
+		cout << "\n\n 1. Sell items";
+		cout << "\n\n 2. Create items ";
+		cout << "\n\n 3. Restock items";
+		cout << "\n\n 4. Update Stock quantity ";
+		cout << "\n\n 5. Display items";
+		cout << "\n\n 6. Items menu ";
+		cout << "\n\n 7. Exit";
+		cout << "\n\n please select (1-7) = ";
 		cin >> ch;
 		switch (ch)
 		{
 		case '1': 
-		 system("clear");
-	    place_order();
-		break;
+		    // system("clear"); 
+	         place_order(); break;
 
 		case '2':
-			
-	    break;
+			write_file();  break;
 
-		case '3':
-			
-	    break;
+		case '3':  
+		     break;
 
-		case '4':
-			
-	    break;
+		case '4':         
+                   		break;
 
-		case '5':
-			
-	    break;
+		case '5': display_all();
+                        break;
 
-		case '6':
-			
-	    break;
+		case '6':       menu();
+	                   break;
+	
+	    case '7': 	int main();
+	                   break;
 		
-	    case '7':
-			exit(0);
-	    break;
-		
-		default:
-			cout << "\a";
-		}
+}
+	} while(ch!='7');
 		return 0;
-	} 
+	 
 
+	}
